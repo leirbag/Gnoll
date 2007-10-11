834a2758349cb28b52a569bc937e152c51611d55
Paf <paf@tuxfamily.org>
2007-10-11 08:59:48 +0000

    2007-10-11  Paf <paf@tuxfamily.org> 	* include/persistentobject.h: Add method getAttributesNames(); Make 	const_iterator on attributes possible 	* src/persistentobject.cpp: Add method getAttributesNames(); Make 	const_iterator on attributes possible

--- trunk/src/core/include/persistentobject.h
+++ trunk/src/core/include/persistentobject.h
@@ -28,8 +28,15 @@
 |                          - Paf - Implements                               |
 |                                     PersistentObject::serializeXML()      |
 |               08/10/2007 - Paf - Add Doxygen comments                     |
+|               10/11/2007 - Paf - Add method getAttributesNames()          |
+|               						- Make const_iterator on attributes        |
+|               						    possible                               |
 |                                                                           |
 \*-------------------------------------------------------------------------*/
+
+
+#ifndef __PERSISTENTOBJECT_H__
+#define __PERSISTENTOBJECT_H__
 
 
 #include <boost/shared_ptr.hpp>
@@ -41,121 +48,150 @@
 #include "iattributehandler.h"
 #include "attributehandlerregistry.h"
 
-#ifndef __PERSISTENTOBJECT_H__
-#define __PERSISTENTOBJECT_H__
+#include "list.h"
+#include "string.h"
+
 
 using namespace std;
 using namespace boost;
 
-
-/**
- *	This is a Persistent Object.
- *	This object maps a unicode string to any object inheriting from IAttribute (ie is (de)serializable).<br/>
- * Thus we can store any data or game object through a PersistentObject (as long as it inherits from IAttribute).<br/>
- * Each PersistentObject is referenced via its instance name.
- * @see IAttribute
- */ 
-class PersistentObject : public IAttribute
+namespace Gnoll
 {
-	private:	
-		typedef map< Glib::ustring, shared_ptr<IAttribute> > mapAttributes;
+	namespace Core
+	{
 
 		/**
-		 * This maps all the attributes to their names
-		 */
-		mapAttributes m_attributes;
-
-		/**
-		 * This is the instance name
-		 */
-		Glib::ustring m_instance;
-
-
-	public:
-
-		/**
-		 * This is the constructor.
-		 * @param _instance This is the instance name; the PersistentObject's name
-		 */
-		PersistentObject(Glib::ustring _instance);
-
-		/**
-		 * This is the destructor
-		 */
-		virtual ~PersistentObject();
-
-
-		/**
-		 * This sets an attribute
-		 * @param _name The name of the attribute
-		 * @param _value The attribute itself
-		 */
-		void setAttribute( const Glib::ustring _name, shared_ptr<IAttribute> _value );
-
-
-		/**
-		 * This return an attribute 
-		 * @param _name The name of the attribute we want
-		 * @return The attribute
-		 */
-		template< class T > shared_ptr<T> getAttribute( const Glib::ustring _name  )
+		 *	This is a Persistent Object.
+		 *	This object maps a unicode string to any object inheriting from IAttribute (ie is (de)serializable).<br/>
+		 * Thus we can store any data or game object through a PersistentObject (as long as it inherits from IAttribute).<br/>
+		 * Each PersistentObject is referenced via its instance name.
+		 * @see IAttribute
+		 */ 
+		class PersistentObject : public IAttribute
 		{
+			public: 
 
-			mapAttributes::iterator iter = m_attributes.find(_name);
+				typedef map< Glib::ustring, shared_ptr<IAttribute> > mapAttributes;
+
+
+			private:	
+
+				/**
+				 * This maps all the attributes to their names
+				 */
+				mapAttributes m_attributes;
+
+				/**
+				 * This is the instance name
+				 */
+				Glib::ustring m_instance;
+
+
+			public:
+
+
+				/**
+				 * This is the constructor.
+				 * @param _instance This is the instance name; the PersistentObject's name
+				 */
+				PersistentObject(Glib::ustring _instance);
+
+				/**
+				 * This is the destructor
+				 */
+				virtual ~PersistentObject();
+
+
+				/**
+				 * This sets an attribute
+				 * @param _name The name of the attribute
+				 * @param _value The attribute itself
+				 */
+				void setAttribute( const Glib::ustring _name, shared_ptr<IAttribute> _value );
+		
+
+				/**
+				 * This return an attribute 
+				 * @param _name The name of the attribute we want
+				 * @return The attribute
+				 */
+				template< class T > shared_ptr<T> getAttribute( const Glib::ustring _name  )
+				{
 	
-			// If there is no attribute with such a name, throw an exception.
-			if( iter == m_attributes.end() ) {
-				throw(Glib::ustring("Attribut [") + _name + Glib::ustring("] non trouve !"));
-			}
-
-
-			shared_ptr<IAttribute> value = iter->second;	
-			return dynamic_pointer_cast<T>(value);
-		}
-
-
-		/**
-		 * Check if an attribute exists
-		 * @param _name Name of the attribute
-		 * @return It returns true if an attribute with such a name exists, or false if it doesn't
-		 */
-		bool hasAttribute ( Glib::ustring _name );
-
-
-		/**
-		 * Delete an attribute
-		 * @param _name Name of the attribute to delete
-		 */
-		void deleteAttribute ( Glib::ustring _name );
-
-
-		/**
-		 * Delete all the attributes
-		 */
-		void deleteAllAttributes ( void ) ;
+					mapAttributes::iterator iter = m_attributes.find(_name);
 	
-
-		/**
-		 * Get the instance name of the PersistentObject
-		 * @return The instance name
-		 */
-		string getInstance();
+					// If there is no attribute with such a name, throw an exception.
+					if( iter == m_attributes.end() ) {
+						throw(Glib::ustring("Attribut [") + _name + Glib::ustring("] non trouve !"));
+					}
 
 
-		/**
-		 * This method serialize the PersistentObject and all its attributes
-		 *
-		 * @return This return the PersistentObject as a XML tree 
-		 */
-		virtual shared_ptr<xmlpp::Document> serializeXML(); 
+					shared_ptr<IAttribute> value = iter->second;	
+					return dynamic_pointer_cast<T>(value);
+				}
+
+	
+				/**
+				 * Check if an attribute exists
+				 * @param _name Name of the attribute
+				 * @return It returns true if an attribute with such a name exists, or false if it doesn't
+				 */
+				bool hasAttribute ( Glib::ustring _name );
 
 
-		/**
-		 * This method deserialize the PersistentObject and all its attributes from a XML element
-		 *
-		 * @param _element The XML Element to parse data from 
-		 */
-		virtual void deSerializeXML( xmlpp::Element* _element );		
-};
+				/**
+				 * Delete an attribute
+				 * @param _name Name of the attribute to delete
+				 */
+				void deleteAttribute ( Glib::ustring _name );
+
+
+				/**
+				 * Delete all the attributes
+				 */
+				void deleteAllAttributes ( void ) ;
+
+				/**
+				 * Get a list of attributes names
+				 * @return A list of all attribute's names
+				 */
+				List getAttributesNames();
+
+				/**
+				 * Get a const_iterator on the first element of the map of the attributes
+				 * @return const_iterator at the beginning of the map of attributes
+				 */
+				mapAttributes::const_iterator begin() const;
+
+				/**
+				 * Get a const_iterator on the end of the map of the attributes
+				 * @return const_iterator at the end of the map of attributes
+				 */
+				mapAttributes::const_iterator end() const;
+
+				/**
+				 * Get the instance name of the PersistentObject
+				 * @return The instance name
+				 */
+				string getInstance();
+
+
+				/**
+				 * This method serialize the PersistentObject and all its attributes
+				 *
+				 * @return This return the PersistentObject as a XML tree 
+				 */
+				virtual shared_ptr<xmlpp::Document> serializeXML(); 
+		
+		
+				/**
+				 * This method deserialize the PersistentObject and all its attributes from a XML element
+				 *
+				 * @param _element The XML Element to parse data from 
+				 */
+				virtual void deSerializeXML( xmlpp::Element* _element );		
+		};
+	}
+}
 
 #endif // __PERSISTENTOBJECT_H__
--- trunk/src/core/src/persistentobject.cpp
+++ trunk/src/core/src/persistentobject.cpp
@@ -25,6 +25,9 @@
 |               07/09/2007 - Paf - Initial release                          |
 |               08/18/2007 - Paf - Update comments                          |
 |               09/25/2007 - Paf - Replace namespace Viracocha by Gnoll     |
+|               10/11/2007 - Paf - Add method getAttributesNames()          |
+|               						- Make const_iterator on attributes        |
+|               						    possible                               |
 |                                                                           |
 \*-------------------------------------------------------------------------*/
 
@@ -32,171 +35,203 @@
 #include "../include/persistentobject.h"
 
 
-PersistentObject::PersistentObject(Glib::ustring _instance) : m_instance(_instance)
+namespace Gnoll
 {
-}
-
-
-PersistentObject::~PersistentObject() 
-{
-}
-
-void PersistentObject::setAttribute( const Glib::ustring _name, shared_ptr<IAttribute> _value )
-{
-	m_attributes[_name] = _value;
-}
-
-bool PersistentObject::hasAttribute ( Glib::ustring _name )
-{
-	mapAttributes::iterator iter = m_attributes.find(_name);
-	
-	return iter != m_attributes.end() ;
-}
-
-
-void PersistentObject::deleteAttribute ( Glib::ustring _name )
-{
-	mapAttributes::iterator iter = m_attributes.find(_name);
-	
-	if( iter == m_attributes.end() ) {
-		return;
-	}
-	
-	m_attributes.erase(iter);
-}
-
-void PersistentObject::deleteAllAttributes ( void ) 
-{
-	m_attributes.clear();
-}		  
-
-string PersistentObject::getInstance()
-{
-	return m_instance;
-}
-
-															 
-shared_ptr<xmlpp::Document> PersistentObject::serializeXML() 
-{
-	// First a new document is created
-	shared_ptr<xmlpp::Document> document( new xmlpp::Document("1.0"));  
-
-	// The root node will contain the whole object
-	xmlpp::Element* root = document->create_root_node("persistentobject");
-	root->set_attribute("instance", this->getInstance());
-
-
-	// Each attribute will be serialiazed and attached to the root node
-	mapAttributes::iterator iter;   
-	for( iter = m_attributes.begin(); iter != m_attributes.end(); iter++ ) {
-
-		// First we set up a node with the informations about the attribute
-		// ex : instance name
-		xmlpp::Element* child = root->add_child("attribute");
-		child->set_attribute("name", iter->first);
-
-		// Then the content of the serialized attribute is attached
-		// as a child to the attribute node
-		// So we will be able to add some information about the attribute 
-		// without being worried of messing around
-		shared_ptr<IAttribute> attr = iter->second;
-		shared_ptr<xmlpp::Document> serializedAttr = attr->serializeXML();
-		child->import_node(serializedAttr->get_root_node());
-	}
-
-
-	return document;
-}		
-
-
-
-
-
-/**
- * Example of a serialized PersistentObject :
- *
- * <persistentobject instance="zelda">
- *   <attribute name="age">
- *     <integer value="345"/>
- *   </attribute>
- *   <attribute name="phrase">
- *     <string value="c'etait un cafard qui vivait dans le noir"/>
- *   </attribute>
- *   <attribute name="pi">
- *     <float value="3.145"/>
- *   </attribute>
- * </persistentobject>
- *
- */
-void PersistentObject::deSerializeXML( xmlpp::Element* _element )
-{
-
-	if (_element	== NULL)
+	namespace Core
 	{
-		return;
-	}
 
-	if (_element->get_name() != "persistentobject")
-	{
-		return;
-	}
-
-	xmlpp::Attribute* instanceName = _element->get_attribute("instance");
-	if(instanceName)
-	{
-		this->m_instance = instanceName->get_value();
-	}
-
-	xmlpp::Node::NodeList nodeList = _element->get_children("attribute");
-
-	// Looping through all attributes
-	for(xmlpp::Node::NodeList::iterator it = nodeList.begin(); it != nodeList.end(); it++)
-	{
-		xmlpp::Attribute* attr = NULL;
-
-		xmlpp::Element* tmpElement = dynamic_cast<xmlpp::Element*>(*it);
-		if (tmpElement)
-		{
-			attr = tmpElement->get_attribute("name");
+		PersistentObject::PersistentObject(Glib::ustring _instance) : m_instance(_instance)
+		{		
 		}
 
-		// There must be an attribute name
-		if (attr != NULL)
+
+		PersistentObject::~PersistentObject() 
 		{
-			string attrName = attr->get_value();
+		}
+
+		void PersistentObject::setAttribute( const Glib::ustring _name, shared_ptr<IAttribute> _value )
+		{
+			m_attributes[_name] = _value;
+		}
+
+
+		bool PersistentObject::hasAttribute ( Glib::ustring _name )
+		{
+			mapAttributes::iterator iter = m_attributes.find(_name);
 			
-			xmlpp::Node::NodeList childrenList = (*it)->get_children();
+			return iter != m_attributes.end() ;
+		}
 
-			// Looping through all attributes' children
-			// Only the last child will be taken in account and associated to the attribute name
-			for(xmlpp::Node::NodeList::iterator itChildren = childrenList.begin(); itChildren != childrenList.end(); itChildren++)
-			{
-				// Get the type of the attribute
-				string name= (*itChildren)->get_name();
 
-				xmlpp::Element* elementChild = dynamic_cast<xmlpp::Element*>(*itChildren);
-
-				if (elementChild)
-				{
-					// We need a handler for this attribute
-					Gnoll::Core::AttributeHandlerRegistry* registry = Gnoll::Core::AttributeHandlerRegistry::getInstancePtr();
-
-					shared_ptr<Gnoll::Core::IAttributeHandler> handler = registry->getHandler(name);
+		void PersistentObject::deleteAttribute ( Glib::ustring _name )
+		{
+			mapAttributes::iterator iter = m_attributes.find(_name);
 	
-					// Check if a handler is available for this attribute
-					if (handler.get() != NULL)
-					{
-						// The handler returns an shared_pointer to a new IAttribute
-						shared_ptr<IAttribute> attribute = handler->handle(elementChild);
+			if( iter == m_attributes.end() ) {
+				return;
+			}
+	
+			m_attributes.erase(iter);
+		}
 
-						// This new IAttribute is associated to the attribute name
-						this->setAttribute(attrName, attribute);
+
+		void PersistentObject::deleteAllAttributes ( void ) 
+		{
+			m_attributes.clear();
+		}		  
+
+
+		List PersistentObject::getAttributesNames()
+		{
+			List attrNames;
+			for( mapAttributes::const_iterator iter = m_attributes.begin(); iter != m_attributes.end(); iter++)
+			{ 
+				shared_ptr<String> attrName (new String((*iter).first));
+				attrNames.push_back( attrName );	
+			}
+
+			return attrNames;
+		}
+
+
+		PersistentObject::mapAttributes::const_iterator PersistentObject::begin() const 
+		{
+			return m_attributes.begin();
+		}
+
+
+		PersistentObject::mapAttributes::const_iterator PersistentObject::end() const
+		{
+			return m_attributes.end();
+		}
+
+
+		string PersistentObject::getInstance()
+		{
+			return m_instance;
+		}
+
+															 
+		shared_ptr<xmlpp::Document> PersistentObject::serializeXML() 
+		{
+			// First a new document is created
+			shared_ptr<xmlpp::Document> document( new xmlpp::Document("1.0"));  
+
+			// The root node will contain the whole object
+			xmlpp::Element* root = document->create_root_node("persistentobject");
+			root->set_attribute("instance", this->getInstance());
+	
+
+			// Each attribute will be serialiazed and attached to the root node
+			mapAttributes::iterator iter;   
+			for( iter = m_attributes.begin(); iter != m_attributes.end(); iter++ ) {
+
+				// First we set up a node with the informations about the attribute
+				// ex : instance name
+				xmlpp::Element* child = root->add_child("attribute");
+				child->set_attribute("name", iter->first);
+
+				// Then the content of the serialized attribute is attached
+				// as a child to the attribute node
+				// So we will be able to add some information about the attribute 
+				// without being worried of messing around
+				shared_ptr<IAttribute> attr = iter->second;
+				shared_ptr<xmlpp::Document> serializedAttr = attr->serializeXML();
+				child->import_node(serializedAttr->get_root_node());
+			}
+
+
+			return document;
+		}		
+
+
+
+		/**
+		 * Example of a serialized PersistentObject :
+		 *
+		 * <persistentobject instance="zelda">
+		 *   <attribute name="age">
+		 *     <integer value="345"/>
+		 *   </attribute>
+		 *   <attribute name="phrase">
+		 *     <string value="c'etait un cafard qui vivait dans le noir"/>
+		 *   </attribute>
+		 *   <attribute name="pi">
+		 *     <float value="3.145"/>
+		 *   </attribute>
+		 * </persistentobject>
+		 *
+		 */
+		void PersistentObject::deSerializeXML( xmlpp::Element* _element )
+		{
+
+			if (_element	== NULL)
+			{
+				return;
+			}			
+
+			if (_element->get_name() != "persistentobject")
+			{
+				return;
+			}
+
+			xmlpp::Attribute* instanceName = _element->get_attribute("instance");
+			if(instanceName)
+			{
+				this->m_instance = instanceName->get_value();
+			}
+
+			xmlpp::Node::NodeList nodeList = _element->get_children("attribute");
+
+			// Looping through all attributes
+			for(xmlpp::Node::NodeList::iterator it = nodeList.begin(); it != nodeList.end(); it++)
+			{
+				xmlpp::Attribute* attr = NULL;
+		
+				xmlpp::Element* tmpElement = dynamic_cast<xmlpp::Element*>(*it);
+				if (tmpElement)
+				{
+					attr = tmpElement->get_attribute("name");
+				}
+
+				// There must be an attribute name
+				if (attr != NULL)
+				{
+					string attrName = attr->get_value();
+			
+					xmlpp::Node::NodeList childrenList = (*it)->get_children();
+		
+					// Looping through all attributes' children
+					// Only the last child will be taken in account and associated to the attribute name
+					for(xmlpp::Node::NodeList::iterator itChildren = childrenList.begin(); itChildren != childrenList.end(); itChildren++)
+					{
+						// Get the type of the attribute
+						string name= (*itChildren)->get_name();
+		
+						xmlpp::Element* elementChild = dynamic_cast<xmlpp::Element*>(*itChildren);
+		
+						if (elementChild)
+						{
+							// We need a handler for this attribute
+							Gnoll::Core::AttributeHandlerRegistry* registry = Gnoll::Core::AttributeHandlerRegistry::getInstancePtr();
+		
+							shared_ptr<Gnoll::Core::IAttributeHandler> handler = registry->getHandler(name);
+		
+							// Check if a handler is available for this attribute
+							if (handler.get() != NULL)
+							{
+								// The handler returns an shared_pointer to a new IAttribute
+								shared_ptr<IAttribute> attribute = handler->handle(elementChild);
+	
+								// This new IAttribute is associated to the attribute name
+								this->setAttribute(attrName, attribute);
+							}
+						}
 					}
 				}
 			}
-		}
+		}		
 	}
-}		
-
+}
 
 
