31a3ca706d319e08b7515c3bdd7cd665e2b0bf93
Bruno Mahé <bruno@gnoll.org>
2008-09-20 18:14:10 -0700

    Remove the old scene parser files from scons project

--- trunk/scons/BaseGnollBuilder.py
+++ trunk/scons/BaseGnollBuilder.py
@@ -262,8 +262,6 @@ class BaseGnollBuilder:
 
 		graphicSourceFiles = [
 			'src/graphic/src/cgraphicmodule.cpp',
-			'src/graphic/src/cscenefactorystd.cpp',
-			'src/graphic/src/csceneparser.cpp',
 			'src/graphic/src/cceguilogger.cpp',
 			]
 
