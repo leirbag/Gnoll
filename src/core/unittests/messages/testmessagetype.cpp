bc2b1baac2c0a929617673633c77c59bef425298
Sylvain Glaize <murdock@celes.(none)>
2009-09-16 00:23:48 +0200

    Added tests for MessageType. Passing.

--- src/core/unittests/messages/testmessagetype.cpp
+++ src/core/unittests/messages/testmessagetype.cpp
@@ -1,10 +1,59 @@
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
 
-// What to test
-// - copy constructible
-// - assignment
-// - equality
-// - unequality
-// - relation order
-// - get string
+#include "../../messages/include/messagetype.h"
 
-// No mock object needed
+BOOST_AUTO_TEST_SUITE(TestMessageType)
+
+using namespace Gnoll::Core::Messages;
+
+static const std::string TYPENAME_ONE("randomType");
+static const std::string TYPENAME_TWO("anotherType");
+
+BOOST_AUTO_TEST_CASE(is_copy_constructible)
+{
+    MessageType type(TYPENAME_ONE);
+    MessageType copiedType(type);
+
+    BOOST_CHECK(type == copiedType);
+}
+
+BOOST_AUTO_TEST_CASE(can_be_assigned)
+{
+    MessageType type(TYPENAME_ONE);
+    MessageType copiedType(TYPENAME_TWO);
+
+    copiedType = type;
+
+    BOOST_CHECK(type == copiedType);
+}
+
+BOOST_AUTO_TEST_CASE(inequality_is_not_equality)
+{
+    MessageType type(TYPENAME_ONE);
+    MessageType otherType(TYPENAME_TWO);
+
+    BOOST_CHECK(type != otherType);
+    BOOST_CHECK(!(type == otherType));
+}
+
+BOOST_AUTO_TEST_CASE(type_can_deliver_type_as_string)
+{
+    MessageType type(TYPENAME_ONE);
+    MessageType otherType(TYPENAME_TWO);
+
+    BOOST_CHECK_EQUAL(TYPENAME_ONE, type.getTypeStr());
+    BOOST_CHECK_EQUAL(TYPENAME_TWO, otherType.getTypeStr());
+}
+
+BOOST_AUTO_TEST_CASE(order_relation_is_string_relation)
+{
+    MessageType type(TYPENAME_ONE);
+    MessageType otherType(TYPENAME_TWO);
+
+    BOOST_CHECK(TYPENAME_TWO < TYPENAME_ONE);
+    BOOST_CHECK(otherType < type);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
