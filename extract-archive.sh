f2c16f3f919edcbe158f5964ec15eb8dcf010f5c
Bruno Mahé <bruno@gnoll.org>
2009-11-10 18:50:39 -0800

    Summary: Enhance extract-archive.sh:   - Make sure there is at least one argument (others are ignored)   - Make the script accept one argument used as the revision to extract (ex: HEAD, or a tag or branch)

--- extract-archive.sh
+++ extract-archive.sh
@@ -1,2 +1,19 @@
-#!/bin/sh
-git archive --format=tar --prefix=gnoll-0.1.7/ HEAD | gzip >gnoll-0.1.7.tar.gz
+#!/bin/bash
+
+# First we make sure we got the revision
+if [ $# -lt 1 ]
+then
+	echo "You need to pass the revision you wish to extract"
+	exit -1
+fi
+
+
+VERSION=$1
+NAME=gnoll
+
+COMMAND="git archive --format=tar --prefix=${NAME}-${VERSION}/ ${VERSION} | gzip > ${NAME}-${VERSION}.tar.gz"
+
+
+echo "$COMMAND"
+
+eval $COMMAND
