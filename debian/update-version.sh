6494d8b7e9ed81da0dd565b507518d78ff2131af
Bruno Mahé <bruno@gnoll.org>
2009-11-15 19:59:30 -0800

    Summary: Append a 0 to version string in debian/update-version.sh if there isn't any.   Versions MUST contain a digit when building a deb. Otherwise deb construction would fail.

--- debian/update-version.sh
+++ debian/update-version.sh
@@ -3,7 +3,7 @@
 # First we make sure we got the revision
 if [ $# -lt 3 ]
 then
-	echo "You need at least 4 parameters: oldversion newversion inputfile"
+	echo "You need at least 3 parameters: oldversion newversion inputfile"
 	exit -1
 fi
 
@@ -11,6 +11,11 @@ OLD_VERSION=$1
 NEW_VERSION=$2
 INPUT_FILE=$3
 OUTPUT_FILE="$3.tmp"
+
+case $NEW_VERSION in
+*[0-9]*);;
+*)NEW_VERSION="${NEW_VERSION}0";;
+esac
 
 COMMAND1="head -n 1 $INPUT_FILE | sed -e \"s/${OLD_VERSION}/${NEW_VERSION}/\" > $OUTPUT_FILE"
 COMMAND2="tail -n +2 $INPUT_FILE >> $OUTPUT_FILE" 
